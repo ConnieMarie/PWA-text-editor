9a78f6d1a5cdb05378ec40a69802de6a9312892f
ConnieMarie <constance.marie.strahan@gmail.com>
2022-11-21 18:07:38 -0600

    install functionality/troubleshooting console error

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate_db', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -14,10 +14,10 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('notes', 'readwrite');
-  const store = tx.objectStore('notes');
+  console.log('POST to the database');
+  const jateDb = await openDB('jate_db', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   const request = store.add({ note: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -26,9 +26,9 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('notes', 'readonly');
-  const store = tx.objectStore('notes');
+  const jateDb = await openDB('jate_db', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
